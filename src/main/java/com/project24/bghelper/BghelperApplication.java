dfde0dbdc252e96d1c7f0b437640f096209a917b
jwilun <jwilun@stud.hs-heilbronn.de>
2024-06-29 16:19:31 +0200

    springboot initialized

---
+++ src/main/java/com/project24/bghelper/BghelperApplication.java
@@ -0,0 +1,13 @@
+package com.project24.bghelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BghelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BghelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project24/bghelper/BghelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project24.bghelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BghelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
