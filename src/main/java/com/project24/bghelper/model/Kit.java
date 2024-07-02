a0af0a177b056438fd0b2ef93ec490b30a89436f
jwilun <jwilun@stud.hs-heilbronn.de>
2024-07-02 08:29:00 +0200

    kit @Document added

--- src/main/java/com/project24/bghelper/model/Kit.java
+++ src/main/java/com/project24/bghelper/model/Kit.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(collection = "kits")
 public class Kit {
   @Id
   private String id;
