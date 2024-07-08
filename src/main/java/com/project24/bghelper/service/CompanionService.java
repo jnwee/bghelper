aa61ad9a22e1b2d50f0978ace1ab84a40b7b2050
jwilun <jwilun@stud.hs-heilbronn.de>
2024-07-08 19:48:12 +0200

    Classes implementation of evil, good and neutral Companions

--- src/main/java/com/project24/bghelper/service/CompanionService.java
+++ src/main/java/com/project24/bghelper/service/CompanionService.java
@@ -1,7 +1,9 @@
 package com.project24.bghelper.service;
 
+import com.project24.bghelper.model.Alignment;
 import com.project24.bghelper.model.Companion;
 import com.project24.bghelper.repository.CompanionRepository;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.slf4j.Logger;
@@ -16,9 +18,18 @@ public class CompanionService {
 
   private CompanionRepository companionRepository;
 
+  private List<Companion> goodCompanions;
+  private List<Companion> neutralCompanions;
+  private List<Companion> evilCompanions;
+
   @Autowired
   public CompanionService(CompanionRepository companionRepository) {
     this.companionRepository = companionRepository;
+    goodCompanions = new ArrayList<>();
+    neutralCompanions = new ArrayList<>();
+    evilCompanions = new ArrayList<>();
+
+    sync();
   }
 
   public List<Companion> getAllCompanions() {
@@ -36,5 +47,46 @@ public class CompanionService {
   public void deleteCompanion(String id) {
     companionRepository.deleteById(id);
     logger.info("Deleted Companion with ID - {}", id);
+  }
+
+  public List<Companion> getGoodCompanions() {
+    return goodCompanions;
+  }
+
+  public List<Companion> getNeutralCompanions() {
+    return neutralCompanions;
+  }
+
+  public List<Companion> getEvilCompanions() {
+    return neutralCompanions;
+  }
+
+  /**
+   * synchronizes the Lists for good, neutral & evil Companions.
+   * more efficient than creating a new List on every get I'd say
+   */
+  public void sync() {
+    goodCompanions.clear();
+    neutralCompanions.clear();
+    evilCompanions.clear();
+
+    List<Companion> allCompanions = companionRepository.findAll();
+    for (Companion companion : allCompanions) {
+      if (companion.getAlignment() == Alignment.CHAOTIC_EVIL ||
+          companion.getAlignment() == Alignment.NEUTRAL_EVIL ||
+          companion.getAlignment() == Alignment.LAWFUL_EVIL) {
+        evilCompanions.add(companion);
+      }
+      if (companion.getAlignment() == Alignment.CHAOTIC_NEUTRAL ||
+          companion.getAlignment() == Alignment.NEUTRAL ||
+          companion.getAlignment() == Alignment.LAWFUL_NEUTRAL) {
+        neutralCompanions.add(companion);
+      }
+      if (companion.getAlignment() == Alignment.CHAOTIC_GOOD ||
+          companion.getAlignment() == Alignment.NEUTRAL_GOOD ||
+          companion.getAlignment() == Alignment.LAWFUL_GOOD) {
+        goodCompanions.add(companion);
+      }
+    }
   }
 }
