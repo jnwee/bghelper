cab6f688f8b3a01c6638a0ab098b39dfd834c977
jwilun <jwilun@stud.hs-heilbronn.de>
2024-07-08 20:08:31 +0200

    companionlists in service implemented for alignments and titles

--- src/main/java/com/project24/bghelper/service/CompanionService.java
+++ src/main/java/com/project24/bghelper/service/CompanionService.java
@@ -18,16 +18,26 @@ public class CompanionService {
 
   private CompanionRepository companionRepository;
 
-  private List<Companion> goodCompanions;
-  private List<Companion> neutralCompanions;
-  private List<Companion> evilCompanions;
+  private List<Companion> companionsBg1;
+  private List<Companion> companionsBg2;
+  private List<Companion> goodCompanionsBg1;
+  private List<Companion> neutralCompanionsBg1;
+  private List<Companion> evilCompanionsBg1;
+  private List<Companion> goodCompanionsBg2;
+  private List<Companion> neutralCompanionsBg2;
+  private List<Companion> evilCompanionsBg2;
 
   @Autowired
   public CompanionService(CompanionRepository companionRepository) {
     this.companionRepository = companionRepository;
-    goodCompanions = new ArrayList<>();
-    neutralCompanions = new ArrayList<>();
-    evilCompanions = new ArrayList<>();
+    companionsBg1 = new ArrayList<>();
+    companionsBg2 = new ArrayList<>();
+    goodCompanionsBg1 = new ArrayList<>();
+    neutralCompanionsBg1 = new ArrayList<>();
+    evilCompanionsBg1 = new ArrayList<>();
+    goodCompanionsBg2 = new ArrayList<>();
+    neutralCompanionsBg2 = new ArrayList<>();
+    evilCompanionsBg2 = new ArrayList<>();
 
     sync();
   }
@@ -49,16 +59,36 @@ public class CompanionService {
     logger.info("Deleted Companion with ID - {}", id);
   }
 
-  public List<Companion> getGoodCompanions() {
-    return goodCompanions;
+  public List<Companion> getGoodCompanionsBg1() {
+    return goodCompanionsBg1;
   }
 
-  public List<Companion> getNeutralCompanions() {
-    return neutralCompanions;
+  public List<Companion> getNeutralCompanionsBg1() {
+    return neutralCompanionsBg1;
   }
 
-  public List<Companion> getEvilCompanions() {
-    return neutralCompanions;
+  public List<Companion> getEvilCompanionsBg1() {
+    return neutralCompanionsBg1;
+  }
+
+  public List<Companion> getEvilCompanionsBg2() {
+    return evilCompanionsBg2;
+  }
+
+  public List<Companion> getNeutralCompanionsBg2() {
+    return neutralCompanionsBg2;
+  }
+
+  public List<Companion> getGoodCompanionsBg2() {
+    return goodCompanionsBg2;
+  }
+
+  public List<Companion> getCompanionsBg2() {
+    return companionsBg2;
+  }
+
+  public List<Companion> getCompanionsBg1() {
+    return companionsBg1;
   }
 
   /**
@@ -66,26 +96,57 @@ public class CompanionService {
    * more efficient than creating a new List on every get I'd say
    */
   public void sync() {
-    goodCompanions.clear();
-    neutralCompanions.clear();
-    evilCompanions.clear();
+    goodCompanionsBg1.clear();
+    neutralCompanionsBg1.clear();
+    evilCompanionsBg1.clear();
+    goodCompanionsBg2.clear();
+    neutralCompanionsBg2.clear();
+    evilCompanionsBg2.clear();
+
+    setCompanionListsDifferentTitles();
+    setAlignmentLists(companionsBg1, false);
+    setAlignmentLists(companionsBg2, true);
+  }
+
+  private void setCompanionListsDifferentTitles() {
+    companionsBg1.clear();
+    companionsBg2.clear();
 
     List<Companion> allCompanions = companionRepository.findAll();
+
     for (Companion companion : allCompanions) {
+      if (companion.isBg1()) {
+        companionsBg1.add(companion);
+      }
+      if (companion.isBg2()) {
+        companionsBg2.add(companion);
+      }
+    }
+  }
+
+  /**
+   * clean code I guess.
+   *
+   * @param companions List to split into alignments
+   * @param bg         set to true for bg2, false for bg1 & SoD idc
+   */
+  private void setAlignmentLists(List<Companion> companions, boolean bg) {
+    for (Companion companion : companions) {
       if (companion.getAlignment() == Alignment.CHAOTIC_EVIL ||
           companion.getAlignment() == Alignment.NEUTRAL_EVIL ||
           companion.getAlignment() == Alignment.LAWFUL_EVIL) {
-        evilCompanions.add(companion);
+        boolean why = (bg) ? evilCompanionsBg2.add(companion) : evilCompanionsBg1.add(companion);
       }
       if (companion.getAlignment() == Alignment.CHAOTIC_NEUTRAL ||
           companion.getAlignment() == Alignment.NEUTRAL ||
           companion.getAlignment() == Alignment.LAWFUL_NEUTRAL) {
-        neutralCompanions.add(companion);
+        boolean why =
+            (bg) ? neutralCompanionsBg2.add(companion) : neutralCompanionsBg1.add(companion);
       }
       if (companion.getAlignment() == Alignment.CHAOTIC_GOOD ||
           companion.getAlignment() == Alignment.NEUTRAL_GOOD ||
           companion.getAlignment() == Alignment.LAWFUL_GOOD) {
-        goodCompanions.add(companion);
+        boolean why = (bg) ? goodCompanionsBg2.add(companion) : goodCompanionsBg1.add(companion);
       }
     }
   }
