9c901831a89d0fee2f139bf55118650d9f3b11cd
leon16c <121579173+leon16c@users.noreply.github.com>
2024-07-11 12:57:57 +0200

    refactor Alignment to use enum constructor

--- bghelper/src/main/java/com/project24/bghelper/model/Alignment.java
+++ bghelper/src/main/java/com/project24/bghelper/model/Alignment.java
@@ -3,30 +3,24 @@ package com.project24.bghelper.model;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum Alignment {
-  LAWFUL_GOOD,
-  NEUTRAL_GOOD,
-  CHAOTIC_GOOD,
-  LAWFUL_NEUTRAL,
-  NEUTRAL,
-  CHAOTIC_NEUTRAL,
-  LAWFUL_EVIL,
-  NEUTRAL_EVIL,
-  CHAOTIC_EVIL;
+  LAWFUL_GOOD("Lawful Good"),
+  NEUTRAL_GOOD("Neutral Good"),
+  CHAOTIC_GOOD("Chaotic Good"),
+  LAWFUL_NEUTRAL("Lawful Neutral"),
+  NEUTRAL("Neutral"),
+  CHAOTIC_NEUTRAL("Chaotic Neutral"),
+  LAWFUL_EVIL("Lawful Evil"),
+  NEUTRAL_EVIL("Neutral Evil"),
+  CHAOTIC_EVIL("Chaotic Evil");
+
+  private final String str;
+
+  Alignment(String str) {
+    this.str = str;
+  }
 
   @Override
   public String toString() {
-    String str = "";
-    switch(this) {
-      case LAWFUL_GOOD -> str = "Lawful Good";
-      case NEUTRAL_GOOD -> str = "Neutral Good";
-      case CHAOTIC_GOOD -> str = "Chaotic Good";
-      case LAWFUL_NEUTRAL -> str = "Lawful Neutral";
-      case NEUTRAL -> str = "Neutral";
-      case CHAOTIC_NEUTRAL -> str = "Chaotic Neutral";
-      case LAWFUL_EVIL -> str = "Lawful Evil";
-      case NEUTRAL_EVIL -> str = "Neutral Evil";
-      case CHAOTIC_EVIL -> str = "Chaotic Evil";
-    }
     return str;
   }
 
