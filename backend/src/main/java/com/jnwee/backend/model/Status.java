8a697c18b552ba0fec5f6add53df387a02ae4488
jwilun <jwilun@stud.hs-heilbronn.de>
2024-12-19 22:44:45 +0100

    toString removed from Status

--- backend/src/main/java/com/jnwee/backend/model/Status.java
+++ backend/src/main/java/com/jnwee/backend/model/Status.java
@@ -1,14 +1,7 @@
 package com.jnwee.backend.model;
 
-import java.util.Locale;
-
 public enum Status {
     ALIVE,
     DEAD,
-    ASCENDED;
-
-    @Override
-    public String toString() {
-        return this.name().toUpperCase(Locale.GERMAN);
-    }
+    ASCENDED,
 }
