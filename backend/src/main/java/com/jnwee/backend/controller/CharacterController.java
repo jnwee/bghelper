2e55d7a985f96b23af796fede31e284be7cebb28
jwilun <jwilun@stud.hs-heilbronn.de>
2024-12-19 05:50:49 +0100

    unused import removed

--- backend/src/main/java/com/jnwee/backend/controller/CharacterController.java
+++ backend/src/main/java/com/jnwee/backend/controller/CharacterController.java
@@ -12,7 +12,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/characters")
