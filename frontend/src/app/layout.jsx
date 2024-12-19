a072cac3c754afeb9bb41454e5e33f1898dae522
jwilun <jwilun@stud.hs-heilbronn.de>
2024-12-19 18:39:06 +0100

    removed unnecessary files/code

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div className="bg-home">{children}</div>
+        <div>{children}</div>
       </body>
     </html>
   );
